21843591777f6fa7fa6ad0b9996f4b604a0c4d49
Anderswith <anderswith@hotmail.com>
2025-03-06 15:49:49 +0100

    .

--- obj/Debug/net8.0/DevOpsWebApplication.AssemblyInfo.cs
+++ obj/Debug/net8.0/DevOpsWebApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevOpsWebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7df8624a91bacc5a13c364718d8914e37f011682")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7476f7815a616340a789f1650cde2113e192a33d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevOpsWebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevOpsWebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
